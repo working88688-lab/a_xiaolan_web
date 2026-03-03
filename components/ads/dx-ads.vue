1360c06b91b05a6dc2c3237e191a9de55449087f
li jin <dexing.2723@gmail.com>
2026-03-03 14:25:59 +0800

    feat:图标调整

--- components/ads/dx-ads.vue
+++ components/ads/dx-ads.vue
@@ -8,18 +8,24 @@ const props = defineProps<{
   adType?: string
 }>()
 
-// const rowOneItems = computed(() => {
-//   return props.items?.slice(0, 5)
-// })
-// const rowTwoItems = computed(() => {
-//   return props.items?.slice(5, props.items.length) ?? []
-// })
+const rowItemsLength = 6
+const rows = 3
+
+const splitIndex = rowItemsLength * rows
+
+const rowOneItems = computed(() => {
+  return props.items?.slice(0, splitIndex)
+})
+const rowTwoItems = computed(() => {
+  return props.items?.slice(splitIndex, props.items.length) ?? []
+})
 </script>
 
 <template>
   <div :key="props.items?.length" class="py-0.5" @touchstart.stop @touchmove.stop @touchend.stop>
+    {{ props.items?.length }}
     <div class="grid grid-cols-6 gap-x-1 gap-y-0.5">
-      <ad-link v-for="(item, index) in items" :key="item.id" :ad-key="props.adKey" :ad-name="props.adName"
+      <ad-link v-for="(item, index) in rowOneItems" :key="item.id" :ad-key="props.adKey" :ad-name="props.adName"
         :ad-type="props.adType" :index class="my-swipe-item" :data="item">
         <div class="aspect-h-1 aspect-w-1 overflow-hidden rounded">
           <dx-image fit="fill" :src="item.img_url" :alt="item.title" />
@@ -29,7 +35,7 @@ const props = defineProps<{
         </div>
       </ad-link>
     </div>
-    <!-- <MarqueeInfinite v-if="rowTwoItems.length" :items="rowTwoItems" gap="0.25rem" class="mt-1">
+    <MarqueeInfinite v-if="rowTwoItems.length" :items="rowTwoItems" gap="0.25rem" class="mt-1">
       <template #item="{ item, index }">
         <ad-link :key="item.id" :ad-key="props.adKey" :ad-name="props.adName"
           :index="index >= 0 && index < rowTwoItems.length - 1 ? index + 5 : (index % rowTwoItems.length) + 5"
@@ -42,20 +48,20 @@ const props = defineProps<{
           </div>
         </ad-link>
       </template>
-</MarqueeInfinite> -->
+    </MarqueeInfinite>
   </div>
 </template>
 
 <style lang="postcss" scoped>
-/* .ad-swipe-slide,
+.ad-swipe-slide,
 .ad-swipe-item {
-  width: 63px;
-  height: 63px;
+  width: 48px;
+  height: 48px;
 }
 
 :deep(.report-click-item) {
-  width: 63px;
-} */
+  width: 48px;
+}
 
 .marquee {
   --swiper-wrapper-transition-timing-function: linear;
