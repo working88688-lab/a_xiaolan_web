504e856d9eda53af823dd4ed55fdf9dfcffee79f
li jin <dexing.2723@gmail.com>
2026-03-06 18:53:41 +0800

    chore:api域名

--- app.config.ts
+++ app.config.ts
@@ -667,10 +667,10 @@ export default defineAppConfig({
     }
   ],
 
-  lines: ['https://api.fdtpzgd.com/pwa.php'],
+  lines: ['https://api.fsajqwld.com/pwa.php'],
   // 接口配置
   api: {
-    baseURL: 'https://api.fdtpzgd.com/pwa.php',
+    baseURL: 'https://api.fsajqwld.com/pwa.php',
     withCredentials: false,
     timeout: 50000,
     version: '4.9.7',
